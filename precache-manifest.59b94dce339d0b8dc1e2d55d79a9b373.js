9960fb01c0aec4f2a701a03bee65f88bb26a059b
William Fitt <wmfitt@hotmail.com>
2019-05-21 23:02:07 -0600

    Updates

---
+++ precache-manifest.59b94dce339d0b8dc1e2d55d79a9b373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564c8e100cebf6d2c7dfe5e1ffbde6bf",
+    "url": "/e-commerce-store/index.html"
+  },
+  {
+    "revision": "a5b19bd0a7a5d25aee5a",
+    "url": "/e-commerce-store/static/css/main.36f8b99b.chunk.css"
+  },
+  {
+    "revision": "70489245eeb117a228cc",
+    "url": "/e-commerce-store/static/js/2.0ceb3391.chunk.js"
+  },
+  {
+    "revision": "a5b19bd0a7a5d25aee5a",
+    "url": "/e-commerce-store/static/js/main.308ff241.chunk.js"
+  },
+  {
+    "revision": "3301f3ff8795ad8e1919",
+    "url": "/e-commerce-store/static/js/runtime~main.a4a328dc.js"
+  }
+]);
